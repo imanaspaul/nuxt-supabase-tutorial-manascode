2ce65ea8adfc45d5aa4dde16153475b067d2b896
Manas Paul <manas@codelogicx.com>
2021-05-31 19:29:58 +0530

    Initial supabase setup done

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <h1>Nuxt supabse tutorial mansacode</h1>
+    {{ $supabase }}
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
